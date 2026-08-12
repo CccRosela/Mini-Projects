1471a405535e18ae3d0ee24821533805c5081615
Rosela Cocoli <147844613+CccRosela@users.noreply.github.com>
2026-08-12 12:26:47 +0000

    Goodbye miniproject finish (len(list) == 2, to not print a comma when there are only 2 people)

--- Goodbye/bye.py
+++ Goodbye/bye.py
@@ -1,8 +1,35 @@
+from colorist import ColorHex
+from pyfiglet import Figlet
+import random
+
+
+fonts = Figlet().getFonts()
+rand = random.choice(fonts)
+figlet = Figlet()
+
 names = []
 
-# To do:
-    # User types in names, stored to a list, from that list def a function that greets the names all in one line goodbye.
-    # Should look smth like: Goodbye to Name1! (if len(names)=1)
-        #goodbye to name1, and name2! (len(names)=2)
-        # goodbye to name1, name2, and name3! .... 
-        # and so on..
+while True:
+    user = input("Enter a name (or 'exit'): ")
+    if user == "exit":
+        break
+    else:
+        names.append(user.strip().title())
+
+
+if len(names) == 2:
+    figlet.setFont(font= rand)
+    print (f"{ColorHex('#93C572')} {figlet.renderText('Goodbye to')} \n{names[0]} and {names[1]}! {ColorHex.OFF}👋")
+
+else:
+    empty = ""
+    for index, name in enumerate(names, start= 1):
+        if index == len(names) - 1:
+            empty += name + ', and '  # second to last person
+        elif index == len(names):
+            empty += name # index of last person
+        elif index != len(names):
+            empty += name + ', ' # all other people
+
+    figlet.setFont(font= rand)
+    print (f"\n{ColorHex('#93C572')}{figlet.renderText('Goodbye to')} \n{empty}! {ColorHex.OFF}👋")
