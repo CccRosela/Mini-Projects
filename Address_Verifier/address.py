7a234a233910473b5b8ad4d40014dd7a0a867bf5
Rosela Cocoli <147844613+CccRosela@users.noreply.github.com>
2026-08-18 09:58:09 +0200

    Enhance address_val for specific domains

--- Address_Verifier/address.py
+++ Address_Verifier/address.py
@@ -4,13 +4,13 @@ from color import color
 def addressVal(address,name):
     character = [".","@"]
 
-    if any(char in address for char in character):
+    if any(char in address for char in character) and (address.endswith("@gmail.com") or address.endswith("@yahoo.com")):
         print(f"Address: {color.BOLD}{color.UNDERLINE}{address}{color.END} is Valid!")
         print(f"Welcome, {color.DARKCYAN}{name}{color.END}")
     else:
         print(f"{color.RED} Invalid address! {color.END}")
 
-name = input("What is your name?: ")
+name = input("What is your name?: ").title()
 mail = input("What is your email address?: ")
 
 addressVal(mail,name)
