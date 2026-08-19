0740595ea7f707c8fcb4a4d7e88d149410cfb447
Rosela Cocoli <147844613+CccRosela@users.noreply.github.com>
2026-08-19 14:52:14 +0200

    Add tests for email validation functionality
    ### This pytest code is not run yet, next update will provide changes if any errors/improvements need to be made

---
+++ Address_Verifier/test_email.py
@@ -0,0 +1,19 @@
+from address_regex import validate
+
+def main():
+    test_validate()
+
+def test_valid():
+    assert validate('rosela31@unet.com') == 'Valid!'
+    assert validate('r@gmail.com') == 'Valid!'
+    assert validate('rosic23@univie.ac.at') == 'Valid!'
+    assert validate('rosela31@unet.AC.AT') == 'Valid!'
+    assert validate('fire_@yahoo.giz.gov') == 'Valid!'
+
+def test_invalid():
+    assert validate('fire_@yahoo.giz.something') == 'Invalid!
+    assert validate('@unet.com') == 'Invalid!'
+    assert validate('@') == 'Invalid!'
+    assert validate('r31@@unet.com') == 'Invalid!'
+    assert validate('rosela31@unet.univie.al.ac.com') == 'Invalid!'
+    assert validate('rc@.giz') == 'Invalid'
