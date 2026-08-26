c82b2f37d45b6796b78c218484a4558acc52ceeb
Rosela Cocoli <147844613+CccRosela@users.noreply.github.com>
2026-08-26 12:45:20 +0200

    Add tests for CoffeeMachine error handling and functionality
    testing instance with negative number creations (raise negativeerror correctly), insufficient ingredients in machine (raise insufficient error correctly), test make_coffee function if it behaves as wanted

--- Coffee_Maker/test_coffee.py
+++ Coffee_Maker/test_coffee.py
@@ -1,16 +1,50 @@
-import unittest
-from coffee import CoffeeMachine, coffee_choice
+from unittest import TestCase
+from coffee import CoffeeMachine, types_dic, coffee_choice
+from coffee import InsufficientError, NegativeError
 
-class TestCoffeeMachine(unittest.TestCase):
-    def test_machine_creation(self):
-        m = CoffeeMachine()
-        self.assertTrue(m, "Status:\n  Water= 100\n  Coffee= 500\n  Milk= 1000\n")
+class TestClasses(TestCase):
+    def test_negative_error(self):
+        with self.assertRaises(NegativeError):
+            coffee_machine_4 = CoffeeMachine(-20, 500, 100)
+        with self.assertRaises(NegativeError):
+            coffee_machine_5 = CoffeeMachine(0, -5, 100)
+        with self.assertRaises(NegativeError):
+            coffee_machine_6 = CoffeeMachine(100, 0, -1)
+        
 
+    def test_invalid_coffee_type(self):
+        coffee_machine_1 = CoffeeMachine()
+        coffee_type = 'not'
+        
+        with self.assertRaises(ValueError):
+            coffee_machine_1.make_coffee(coffee_type)
+    
+    def test_make_coffee_good(self):
+        coffee_machine_1 = CoffeeMachine()
+        coffee_type = 'cold brew'
 
-"""
-def test_valid_choice():
-    assert coffee_choice() == pass
+        coffee_machine_1.make_coffee(coffee_type)
 
-def test_invalid_choice():
-    assert coffee_choice() == pass
-"""
+        self.assertEqual(coffee_machine_1.water, 90)
+        self.assertEqual(coffee_machine_1.coffee, 420)
+        self.assertEqual(coffee_machine_1.milk, 800)
+
+    def test_ingredients_dont_change_if_insufficient(self):
+        coffee_machine_2 = CoffeeMachine(50, 50, 0)
+        coffee_type = 'latte'
+
+        self.assertEqual(coffee_machine_2.water, 50)
+        self.assertEqual(coffee_machine_2.coffee, 50)
+        self.assertEqual(coffee_machine_2.milk, 0)
+    
+    def test_insufficient_error(self):
+        coffee_machine_3 = CoffeeMachine(0, 0, 0)
+        coffee_type = 'cafe au lait'
+
+        with self.assertRaises(InsufficientError):
+            coffee_machine_3.make_coffee(coffee_type)
+        
+        
+if __name__ == "__main__":
+    import unittest
+    unittest.main()
