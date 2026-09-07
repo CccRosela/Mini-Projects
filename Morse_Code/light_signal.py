0c1b5f2d7ef6c5b65f1d9dbd9f7a8e5a50fab976
Rosela Cocoli <roselacocoli@gmail.com>
2026-09-07 23:51:50 +0200

    add light signal visuals using turtle

---
+++ Morse_Code/light_signal.py
@@ -0,0 +1,16 @@
+import turtle
+import time
+
+screen = turtle.Screen()
+screen.title("Morse Code Light Signal")
+screen.screensize(200, 200)
+screen.bgcolor("black")
+
+# create a turtle object for light signal
+dot = turtle.Turtle()
+dot.shape("circle")
+dot.color("yellow")
+dot.shapesize(7, 7)
+
+
+# screen.getscreen().update()	
